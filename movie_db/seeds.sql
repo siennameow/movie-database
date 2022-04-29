0152b9bca0e10c143a8ec0db06d74d929bdd42cf
Hexuan Li <lihexuan1@gmail.com>
2022-04-29 14:03:09 -0700

    complete database building

---
+++ movie_db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO movies (id, movie_name)
+VALUES (1, "Jumanji"),
+       (2, "Harry Potter"),
+       (3, "Lord of the rings"),
+       (4, "Star Wars"),
+       (5, "Hotel Transylvania");
+
+INSERT INTO reviews (id, movie_id, review)
+VALUES (1, 1,"Like yeah man."),
+       (2, 2,"Holy Wowness!!!!!"),
+       (3, 3,"Fantasy epic"),
+       (4, 4,"This movie was great to watch"),
+       (5, 5,"I LOVE the characters.");
--- server.js
+++ server.js
@@ -13,7 +13,34 @@ const db = mysql.createConnection(
     host: 'localhost',
     user: 'root',
     password: 'password',
-    database: 'movies_db'
+    database: 'movie_db'
   },
   console.log(`Connected to the books_db database.`)
 );
+
+app.get('/api/movies', (req, res) => {
+db.query('SELECT * FROM movies' , function (err, results) {
+    res.json(results);
+  });
+});
+
+app.post('/api/add-movie', (req, res) => {
+    const addMovie = req.body;
+
+});
+
+app.get('/api/update-review', (req, res) => {
+ res.json("woof");
+});
+
+app.put('/api/review/:id', (req, res) => {
+    res.json("quack");
+});
+
+app.delete('/api/movie/:id', (req, res) => {
+ res.json("wooga");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
